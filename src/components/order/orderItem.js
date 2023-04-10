3d409ca36d1a61866bea97768c25d0a1a866d045
PhuocN0v <PhuocN0v@DESKTOP-6RP1Q8F>
2023-04-10 17:10:43 +0700

    order page

---
+++ src/components/order/orderItem.js
@@ -0,0 +1,63 @@
+import { Box, Button, Divider, Flex, Spacer, Text } from '@react-native-material/core'
+import React from 'react'
+import { StyleSheet, useWindowDimensions } from 'react-native'
+import PropTypes from 'prop-types'
+import { Ionicons } from '@expo/vector-icons'
+export default function OrderItem ({ data }) {
+  const { width } = useWindowDimensions()
+  return <Box style={{ width, height: 250, backgroundColor: '#fff', marginTop: 15, padding: 10 }}>
+    <Flex direction='row' style={{ alignItems: 'center', marginVertical: 10 }}>
+      <Text style={data.status === 'Completed' ? style.recivedLabel : style.failedLabel}>{data.status === 'Prepare' ? 'Đã nhận' : 'Thất bại'}</Text>
+      <Spacer />
+      <Text style={style.date}>{data.orderdate}</Text>
+    </Flex>
+    <Flex style={{ alignItems: 'center', marginVertical: 10 }} direction="row">
+      <Ionicons name='home-outline' style={style.icon} />
+      <Text style={style.location}>97 Man thiện, Hiệp Phú, Quận 9, tp Thủ Đức </Text>
+    </Flex>
+    <Divider style={{ height: 3 }} />
+    <Flex style={{ alignItems: 'center', marginVertical: 10 }} direction="row">
+      <Ionicons name='location-outline' style={style.icon} />
+      <Text style={style.location} numberOfLines={1}>{data.address}</Text>
+    </Flex>
+    <Flex direction='row' style={{}}>
+      <Text style={{ fontSize: 14, width: width * 0.7 }}>{data.orderdetail}</Text>
+      <Text style={{ color: '#F6AC31', fontFamily: 'Roboto_700Bold', alignSelf: 'center', justifyContent: 'center', textAlign: 'center' }}>{data.totalprice + ' VND'}</Text>
+    </Flex>
+
+    <Flex direction='row'>
+      <Spacer/>
+      <Button style={{ color: '#fff', backgroundColor: '#F6AC31', width: 175, marginTop: 10 }} title={data.status === 'Prepare' ? 'Huỷ đơn hàng' : 'Đã nhận hàng'} />
+    </Flex>
+  </Box>
+}
+
+const style = StyleSheet.create({
+  recivedLabel: {
+    backgroundColor: '#FFEFD4',
+    color: '#F6AC31',
+    padding: 5,
+    borderRadius: 5
+  },
+  failedLabel: {
+    backgroundColor: '#FFEFD4',
+    color: '#F6AC31',
+    padding: 5,
+    borderRadius: 5
+  },
+  date: {
+    color: '#888'
+  },
+  icon: {
+    fontSize: 24,
+    color: '#888'
+  },
+  location: {
+    color: '#272727',
+    fontSize: 16
+  }
+})
+
+OrderItem.propTypes = {
+  data: PropTypes.object
+}
--- src/screens/orders/order.js
+++ src/screens/orders/order.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, ScrollView, useWindowDimensions } from 'react-native'
+import OrderItem from '../../components/order/orderItem'
 
 export default function OrderPage () {
+  const { width, height } = useWindowDimensions()
+
+  const orders = [
+    {
+      status: 'Prepare',
+      address: '123/89 Võ Văn Ngân, TP Thủ Đức',
+      orderdate: 'Mon, 20 Feb 2023 17:44:01 GMT',
+      totalprice: '200000',
+      orderdetail: 'Trà sữa thập cẩm(x2), Size L, Cafe sữa(X1), Size S, Topping: Trân Châu Trắng, Thạch dừa'
+    },
+    {
+      status: 'Delivering',
+      address: '97 Man Thiện, phường Hiệp Phú, thành phố Thủ Đức, thành phố Hồ Chí Minh',
+      orderdate: 'Wed, 15 Mar 2023 10:02:40 GMT',
+      totalprice: '200000',
+      orderdetail: 'Trà sữa thập cẩm(x2), Size L, Cafe sữa(X1), Size S, Topping: Trân Châu Trắng, Thạch dừa'
+    }
+  ]
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Order Screen</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width, height, backgroundColor: '#ccc' }}>
+      <ScrollView>
+        {orders.map((order, index) => {
+          return <OrderItem data={order} key={index}/>
+        })}
+      </ScrollView>
     </View>
   )
 }
